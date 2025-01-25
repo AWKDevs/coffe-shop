9f690edc3665577520982d26e36dfc4a2711b69e
LOPMIP <59035122+LOPMIP@users.noreply.github.com>
2025-01-25 03:00:18 -0600

    Header + Navbar

---
+++ app/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+import { SiCoffeescript } from "react-icons/si";
+import { BiSolidCart } from "react-icons/bi";
+import { HeaderButtonBronwBorder, HeaderIconButton } from "./HeaderButton";
+
+const Header = () => {
+  return (
+    <header className="bg-[rgb(19,14,1)] text-white shadow-md">
+      <div className="container mx-auto flex items-center justify-between py-4 px-6">
+        <Link href="/" className="text-2xl font-bold">
+          <SiCoffeescript />
+        </Link>
+        <nav className="flex items-center justify-center space-x-6">
+            <HeaderIconButton href="/cart" icon={<BiSolidCart className="text-2xl text-[rgb(249,217,120)]"/>}/>
+            <HeaderButtonBronwBorder href="/menu" insideText="MENÚ"/>
+            <HeaderButtonBronwBorder href="/recommendations" insideText="RECOMENDACIONES"/>
+            <HeaderButtonBronwBorder href="/promotions" insideText="PROMOCIONES"/>
+            <HeaderButtonBronwBorder href="/new" insideText="LO NUEVO"/>
+            <HeaderButtonBronwBorder href="/about" insideText="NOSOTROS"/>
+            <HeaderButtonBronwBorder href="/contact" insideText="CONTÁCTANOS"/>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,10 +1,11 @@
 // app/components/Layout.tsx
+import Header from './Header';
 import Navbar from './Navbar';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <>
-            <Navbar />
+            <Header />
             <main style={{ padding: '20px' }}>
                 <h2>Bienvenido a Coffee Shop</h2>
                 {children}
