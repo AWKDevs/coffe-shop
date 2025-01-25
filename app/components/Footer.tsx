710158858e3f147a4a182a96a861c4b15ba5cf8e
LOPMIP <59035122+LOPMIP@users.noreply.github.com>
2025-01-25 13:58:09 -0600

    Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,27 @@
+import { FaInstagram, FaFacebook, FaTiktok, FaEnvelope, FaYoutube, FaShareAlt } from "react-icons/fa";
+
 const Footer = () => {
     return(
-    <footer>
-
-    </footer>
+        <footer className="bg-[rgb(201,186,175)] text-[rgb(110,80,60)] py-8 px-6 rounded-[20]">
+        <div className="text-center mb-4">
+            <h2 className="font-bold text-lg text-[rgb(67,52,9)]">Contáctanos</h2>
+            <p className="text-sm">
+            Dirección: (Añadir dirección exacta de la cafetería) Teléfono: (Añadir número de contacto) Horario: (Especificar días y horarios de atención)
+            </p>
+        </div>
+        <div className="flex justify-center space-x-6 mb-4">
+            <FaInstagram className="text-2xl cursor-pointer" />
+            <FaFacebook className="text-2xl cursor-pointer" />
+            <FaTiktok className="text-2xl cursor-pointer" />
+            <FaEnvelope className="text-2xl cursor-pointer" />
+            <FaYoutube className="text-2xl cursor-pointer" />
+            <FaShareAlt className="text-2xl cursor-pointer" />
+        </div>
+        <div className="text-center">
+            <p className="text-sm mb-2">Dulces Recuerdos Café - 10 años creando momentos mágicos.</p>
+            <p className="text-sm font-bold">Aviso de Privacidad</p>
+        </div>
+        </footer>
     )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import './styles/globals.css';
 import Navbar from './components/NavBar/ResponsiveNav';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function RootLayout({
     children,
@@ -13,6 +14,7 @@ export default function RootLayout({
             <body>
                 <Header />
                 {children}
+                <Footer />
             </body>
         </html>
     );
